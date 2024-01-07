5ff56d1e46ee5d9c60781cd9ac3e20f092eb934e
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-01-07 11:48:13 +0000

    refactor(apps: starmicro): restructure app structure

--- tests/sample-apps/starmicro-insurerance/starmicro_ussd.ts
+++ tests/sample-apps/starmicro-insurerance/starmicro_ussd.ts
@@ -68,48 +68,7 @@ router
     },
   ]);
 
-// New policy registration, using forms feature
-router
-  .menu(MenuType.customer_new_policy) // menu name becomes form name
-  .isForm()
-  .inputs([
-    {
-      name: "first_name",
-      validate: /[\w\s]{2,}/,
-      display: "Enter first name",
-      next_input: "last_name",
-    },
-    {
-      name: "last_name",
-      validate: /.*/,
-      display: "Enter last name",
-      next_input: "age",
-    },
-    {
-      name: "age",
-      validate: /\d{1,2}/,
-      display: "Enter age (minium 18)",
-      next_input: "pin",
-    },
-    {
-      name: "pin",
-      validate: /\d{4}/,
-      display: "Enter PIN",
-      handler: async (req, session) => {
-        //TODO: add session to request object
-        const form = await session.get(MenuType.customer_new_policy);
-        console.log(form); // Save form to database
-      },
-      end: true, // Ends the form, and navigates to next menu
-      next_menu: "policy_created",
-    },
-  ]);
-
-router
-  .menu("policy_created")
-  .message(
-    "Thank you for registering as customer with Star M Kindly dial short code again to register a policy"
-  );
+import "./menus/client/index";
 
 app.listen(3000, "localhost", () => {
   console.log("Server listening on port 3000");
