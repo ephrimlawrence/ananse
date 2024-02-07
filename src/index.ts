b309d6b459bbe770b55ef32bb12524ff450792ba
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-02-07 17:26:33 +0000

    ci: update for npm publishing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { App } from "./core/app.core";
 export * from "./menus";
 export * from "./types";
+export * from "./sessions";
 
 export { Middleware } from "./middlewares/base.middleware";
 export { DefaultMiddleware } from "./middlewares/default.middleware";
