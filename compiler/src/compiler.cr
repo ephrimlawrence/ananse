a55cd7cb8e93259738144d9df495492448457484
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-07-26 09:08:04 +0000

    feat: add cli args parsing to compiler

--- compiler/src/compiler.cr
+++ compiler/src/compiler.cr
@@ -7,6 +7,8 @@ require "./code_generator.cr"
 require "./ast.cr"
 require "./semantic_analyzer"
 require "./ast_transformer.cr"
+require "option_parser"
+require "option_parser"
 
 # require "./lexer.cr"
 
@@ -14,7 +16,7 @@ module Compiler
   VERSION = "0.1.0"
 
   # @@program : String = ""
-  @@had_error : Bool = false
+  # @@had_error : Bool = false
 
   def self.run
     source : String = File.read("spec/program.ussd")
@@ -22,28 +24,31 @@ module Compiler
     # file.close
 
     # p! @@program
-    scanner : Scanner::Scan = Scanner::Scan.new(source)
-    parser : Parser = Parser.new(scanner.scan_tokens)
-    program : Array(AST::Stmt) = parser.parse
 
     # // Stop if there was a syntax error.
-    if @@had_error
-      return
-    end
+    # if @@had_error
+    #   return
+    # end
 
     # puts program
     # puts AstPrinter.new.print(expression)
 
     begin
+      scanner : Scanner::Scan = Scanner::Scan.new(source)
+
+      parser : Parser = Parser.new(scanner.scan_tokens)
+      program : Array(AST::Stmt) = parser.parse
+
+      p! program
       analyzer : SemanticAnalyzer = SemanticAnalyzer.new(program)
       analyzer.analyze
 
       transformed_ast = AstTransformer.new(program).transform
       code = CodeGenerator.new.generate(transformed_ast)
-      puts code
-      raise CompilerError.new("message")
+      STDOUT.puts code
+      # raise CompilerError.new("message")
     rescue err
-      puts err.message
+      STDERR.puts err.message
     end
     # code_gen.generate(expression.as(Expression::Expr))
 
@@ -70,4 +75,31 @@ module Compiler
 end
 
 # Call run on script execution
-Compiler.run
+# Compiler.run
+
+build : Bool = false
+
+OptionParser.parse do |parser|
+  parser.banner = "Ananse USSD Compiler"
+
+  parser.on "-b", "--build", "Generate Typescript code" do
+    build = true
+    Compiler.run
+  end
+
+  parser.on "-v", "--version", "Show version" do
+    puts "version 1.0"
+    exit
+  end
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
--- compiler/src/error.cr
+++ compiler/src/error.cr
@@ -1,37 +1,35 @@
 class CompilerError < Exception
-  property token : Token? = nil
-
   def initialize(message : String)
     super(message)
   end
 
   def initialize(message : String, token : Token)
-    super("Error at #{token.location.line}:#{token.location.column}: #{message}")
+    puts "here", message
+    if (token.type == TokenType::EOF)
+      super report(token.location, " at end", message)
+    else
+      super report(token.location, " at '" + token.value + "'", message)
+    end
   end
 
   def initialize(location : Location, message : String)
     super("Error at #{location.line}:#{location.column}: #{message}")
   end
 
-  def self.error(location : Location, message : String)
-    report(location, "", message)
+  def initialize(location : Location, message : String)
+    super report(location, "", message)
   end
 
-  def self.error(token : Token, message : String)
-    if (token.type == TokenType::EOF)
-      report(token.location, " at end", message)
-    else
-      report(token.location, " at '" + token.value + "'", message)
-    end
-  end
+  # def self.error(token : Token, message : String)
+  # end
 
-  def self.error(message : String)
-    if (token.type == TokenType::EOF)
-      report(token.location, " at end", message)
-    else
-      report(token.location, " at '" + token.value + "'", message)
-    end
-  end
+  # def self.error(message : String)
+  #   if (token.type == TokenType::EOF)
+  #     report(token.location, " at end", message)
+  #   else
+  #     report(token.location, " at '" + token.value + "'", message)
+  #   end
+  # end
 
   # runtimeError(RuntimeError error) {
   #   System.err.println(error.getMessage() +
@@ -39,8 +37,8 @@ class CompilerError < Exception
   #   hadRuntimeError = true;
   # }
 
-  def self.report(location : Location, where : String, message : String)
-    puts "[line #{location.line}:#{location.column}] Error #{where} : #{message}"
+  private def report(location : Location, where : String, message : String)
+    return "[line #{location.line}:#{location.column}] Error #{where} : #{message}"
   end
 
   def backtrace?
@@ -49,10 +47,6 @@ class CompilerError < Exception
 
   def backtrace?
     [] of String
-  end
-
-  def to_s(io : IO)
-    io << "Parse Error at "
   end
 end
 
