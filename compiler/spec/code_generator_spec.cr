176b7d247e864404357145dce09f4e8904a7d666
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-08-06 21:53:18 +0000

    ci: pre-commit test

--- compiler/spec/code_generator_spec.cr
+++ compiler/spec/code_generator_spec.cr
@@ -1,22 +1,22 @@
 require "./spec_helper"
 describe CodeGenerator do
-  describe "hello_world.yml" do
-    describe "Displays hello world!" do
-      server : TestDriver? = nil
-      before_all do
-        server = TestDriver.new("hello_world.ts").start
-      end
-
-      after_all do
-        server.as(TestDriver).stop
-      end
-      describe "Hello world!" do
-        it "Displays hello world!" do
-          resp49974 : String? = server.as(TestDriver).input([""])
-          resp49974.nil?.should eq(false)
-          resp49974.as(String).includes?("Hello World").should eq(true)
-        end
-      end
-    end
+describe "hello_world.yml" do
+describe "Displays hello world!" do
+  server : TestDriver? = nil
+  before_all do
+    server = TestDriver.new("hello_world.ts").start
   end
+
+  after_all do
+    server.as(TestDriver).stop
+  end
+  describe "Hello world!" do
+    it "Displays hello world!" do
+resp34454 : String? = server.as(TestDriver).input([""])
+resp34454.nil?.should eq(false)
+resp34454.as(String).includes?("Hello World").should eq(true)
+end
+end
+end
+end
 end
