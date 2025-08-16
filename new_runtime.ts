274d5a11d6a7a82853e694a28c753954d9e96998
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-08-16 08:00:48 +0000

    fix(runtime): bug fixes

--- new_runtime.ts
+++ new_runtime.ts
@@ -8,163 +8,176 @@
 // * save session state
 // * return response
 
+import { Runtime } from "./runtime";
+import { Request, Response, SessionMode } from "./types";
+
 // An in-memory Map to simulate a session database.
 // In a real application, this would be Redis, Firestore, or a similar store.
-const sessions = new Map();
 const menuStack: string[] = [];
 
-/**
- * Saves a session state to the mock database.
- * @param {string} sessionId The unique session ID.
- * @param {object} sessionData The data to store for the session.
- */
-function saveSession(sessionId, sessionData) {
-	sessions.set(sessionId, sessionData);
-}
-
-/**
- * Retrieves a session state from the mock database.
- * @param {string} sessionId The unique session ID.
- * @returns {object | undefined} The session data or undefined if not found.
- */
-function getSession(sessionId) {
-	return sessions.get(sessionId);
-}
-
-function saveSession(request) {
-	// TODO: save session to db/redis/memory/
-}
-
 // Menu api
-function menu_name_function(request, session, type: "get" | "post") {
+async function menu_name_function(
+	runtime: Runtime,
+	// request,
+	// session,
+	type: "get" | "post",
+) {
 	// type -> 'get': only execute logic neccary to display menu text
 	// type -> 'post': execute main logic and return next menu (with get request)
-  let endSession: boolean = false;
-  let input: string = request.input;
-  let message: string = "";
-  const errorMessage: string? = session.menu_error("menu_name");
+	const menuName = "<menu-name-here>";
 
-  if(errorMessage != null){
-      // Add menu name back to stack
-    if(menuStack[0] != "this_menu_name"){
-      menuStack.unshift("this_menu_name");
-    }
+	const errorMessage: string | undefined = await runtime.getError(menuName);
 
-    session.clear_error("menu_name");
-    return errorMessage;
-  }
+	if (errorMessage != null) {
+		// Add menu name back to stack
+		if (runtime.getCurrentMenu() !== menuName) {
+			runtime.setNextMenu(`${menuName}_[GET]`);
+		}
 
-  if (type === "get") {
-		// process display text
-
-    // process input
-    request.session.set("variable_name", request.input);
-
-    // if options are defined
-    // process options
-    let options: {choice: string|RegExp, label: string, goto: string, action: string}[] = []
-    for(const option in options){
-      message += option.label;
-    }
-
-    // Adds item name
-    if(menuStack[0] != "this_menu_name"){
-      menuStack.unshift("this_menu_name");
-    }
-
-    // run logic
-		saveSession();
-
-		// return string or end session
-    // TODO: build_pagination
-		return message;
+		await runtime.clearError(menuName);
+		return errorMessage;
 	}
 
+	// let endSession: boolean = false;
+
+	if (type === "get") {
+		let message: string = "";
+		// process display text
+
+		// NOTE: input, end, goto, option (goto & actions) are not executed in get request
+		// process input
+		// request.session.set("variable_name", runtime.session().userData());
+
+		// if options are defined
+		// process options
+		let options: {
+			choice: string | RegExp;
+			label: string;
+			goto: string;
+			action: string;
+		}[] = [];
+		for (const option in options) {
+			message += option.label;
+		}
+
+		// Adds item name
+		// if (menuStack[0] != "this_menu_name") {
+		// 	menuStack.unshift("this_menu_name");
+		// }
+
+		// run logic
+		await runtime.saveState();
+
+		// return string or end session
+		// TODO: build_pagination
+		return runtime.respond(message);
+	}
+
+	const input: string | null = runtime.session().userData();
+
 	if (type === "post") {
-    // if input is defined
-    session.set('variable', input);
-    const ___variable = input;
+		// if input is defined
+		runtime.setValue("variable-name", input);
+		const ___variable = input;
 
-    // if options are defined
-    // process options
-    let options: {choice: string|RegExp, label: string, goto: string, action: string}[] = []
-    let foundMatch: boolean = false;
-    let selectedOption: string = null;
+		// if options are defined
+		// process options
+		let options: {
+			choice: string | RegExp;
+			label: string;
+			goto: string;
+			action: string;
+		}[] = [];
+		let foundMatch: boolean = false;
+		let selectedOption: string = null;
 
-    for(const option of options){
-      if(!input_matches_choice({input, choice})){
-        foundMatch = true;
-        selectedOption = option.label;
-      }
-    }
+		for (const option of options) {
+			if (!input_matches_choice({ input, choice })) {
+				foundMatch = true;
+				selectedOption = option.label;
+			}
+		}
 
-    if(!foundMatch){
-      // re-render option
-      return menu_name_function(request, session, "get");
-    }
+		if (!foundMatch) {
+			// re-render option
+			return menu_name_function(runtime, "get");
+		}
 
-    // Repeat this for all options defined by the user
-    if(selectedOption == "option_label"){
-      let canNavigate: boolean = true;
-      // if 'goto' & action is set, then 'goto' is called only if action returns 'true'
+		// Repeat this for all options defined by the user
+		if (selectedOption == "option_label") {
+			let canNavigate: boolean = true;
+			// if 'goto' & action is set, then 'goto' is called only if action returns 'true'
 
-      // if 'goto' & 'action' is defined
-      // canNavigate
-      const actionResult = await actionName({args});
-      // for variable
-      session.set('variable_name', actionResult);
+			// if 'goto' & 'action' is defined
+			// canNavigate
+			const actionResult = await actionName({ args });
+			// for variable
+			await runtime.setValue("variable_name", actionResult);
 
-      if(actionResult){
-        menuStack.shift(); // remove current menu from the stack
-        menuStack.unshift("next-menu_name"); // push new menu to the stack
-        saveSession();
+			if (actionResult) {
+				runtime.removeCurrentMenu(); // remove current menu from the stack
 
-        // for goto is set
-        return next_menu_name(request, session, "get");
-      }
-    }
+				// if goto is set
+				await runtime.setNextMenu("next-menu_name_[GET]"); // push new menu to the stack
+				await runtime.saveState();
 
-    // [START] goto
-    // goto code here
-    // if error message is set
-    session.set_menu_error('menu_name', 'error-msg');
+				// for goto is set
+				return next_menu_name(runtime, "get");
+			}
+		}
 
-    // next menu
-    menuStack.shift(); // remove current menu from the stack
-    menuStack.unshift("next-menu_name"); // push new menu to the stack
-    saveSession();
+		// [START] goto
+		// goto code here
+		// if error message is set
+		await runtime.setError("menu_name", "error-msg");
 
-    return next_menu_name(request, session, "get");
-    // [END]
+		// next menu
+		await runtime.removeCurrentMenu(); // remove current menu from the stack
+		runtime.setNextMenu("next-menu_name_[GET]"); // push new menu to the stack
+		await runtime.saveState();
+
+		// for goto is set
+		return next_menu_name(runtime, request, session, "get");
+
+		// return next_menu_name(request, session, "get");
+		// [END]
 		// run logic...
 
+		// START: Session Termination
+		await runtime.endSession();
+		// END: Session Termination
 		// return next menu or end session
 		// return next_menu_function(request, "get");
 	}
 }
 
 // Sample 1
-function handleMenu(request) {
-	switch ("menu_from_session") {
-		case "start":
-			if (menuStack[0] == null) {
-        menuStack.unshift("this-menu-name");
-				return menu_name_function(request, "get");
-			} else if (menuStack[0] == "this-menu-name") {
-				// replace with menu name
-				return menu_name_function(request, "post");
-			}
-			break;
-		case "menu_1":
-			return menu_name_function(request);
-		case "menu_2":
-			return menu_name_function();
+function handleMenu(runtime: Runtime) {
+	const currentMenu: string | undefined = runtime.getCurrentMenu();
+
+	switch (currentMenu) {
+		case "menu_name_[GET]":
+			return menu_name_function(runtime, "get");
+		case "menu_name_[POST]":
+			return menu_name_function(runtime, "post");
+		// case "menu_2":
+		// 	return menu_name_function(request);
+		// case "menu_3":
+		// 	return menu_name_function();
 		default:
-			return "Session cannot be retrieved";
+			if (runtime.session().mode() === SessionMode.start) {
+				runtime.setNextMenu("start_menu_[POST]");
+
+				return "start menu function(get)";
+			}
+
+			runtime.endSession();
+			runtime.respond("Session cannot be retrieved");
 	}
 }
 
-
-export function requestHandler(req, resp){
-  // get session
+export async function requestHandler(req: Request, resp: Response) {
+	const runtime = await new Runtime(req, resp);
+	handleMenu(runtime);
+	// get session
 }
